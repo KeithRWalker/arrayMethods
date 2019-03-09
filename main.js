27c89f755537affa4b9a7b420436639ea7b75f41
Keith Walker <Keithrwalker418@gmail.com>
2019-03-09 10:23:14 -0600

    unloadPassenger() shift/pop

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
 console.log('main.js linked');
 const struggleBus = [];
 
-const addPassenger = (name, wallet, isStruggling, seat) => { //make passenger and seat them on bus
+const addPassenger = (name, wallet, isStruggling, seat) => { //make passenger object// 'seat' them on bus
     const passenger = {
         name: name,
         wallet: wallet,
-        isStruggling: isStruggling,
-        seat: seat
+        isStruggling: isStruggling
     };
 
     if (seat === 'back') {
         struggleBus.push(passenger);
     } else if (seat === 'front'){
         struggleBus.unshift(passenger);
+    };
+};
+
+const unloadPassenger = (bus, seat) => { // remove passenger from bus //return that passenger object
+    if(seat === 'front'){
+        return bus.shift(); //
+    } else if (seat === 'back'){
+        return bus.pop();
     };
 };
 
@@ -25,9 +32,10 @@ const busBuilder = (bus) => {
     let = busString = ' ';
     for (let i=0; i< bus.length; i++){
         busString += `<div class = "busSeat">`;
-        busString +=    `<h4>${bus[i].name}</h4>`;
-        busString +=    `<p>${bus[i].wallet}</p>`;
-        busString +=    `<p>${bus[i].isStruggling}</p>`;
+        busString +=    `<h4>NAME: ${bus[i].name}</h4>`;
+        busString +=    `<p>$$: ${bus[i].wallet}</p>`;
+        busString +=    `<p>STUGGLING?: ${bus[i].isStruggling}</p>`;
+        busString +=    `<hr>`
         busString += `</div>`;
     };
     domPrint('testDiv', busString);
@@ -35,8 +43,15 @@ const busBuilder = (bus) => {
 
 const init = () => {
     addPassenger('Keith', 5, true, 'front');
+    addPassenger('Frankie', 10, true, 'back');
+    addPassenger('Loki', 1, false, 'back');
+    addPassenger('Lame Person', 10000, true, 'front');
+    addPassenger('Other Lame Person', 0, true, 'back');
+
+    const firstPassenger = unloadPassenger(struggleBus, 'front');
+    const secondPassenger = unloadPassenger(struggleBus, 'back');
+
     busBuilder(struggleBus);
-    console.log(struggleBus);
 };
 
 init();
